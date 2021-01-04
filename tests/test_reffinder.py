2318fb9e3f3b1453c8a967168dda6102b297a17c
Luca Wellmeier <luca_wellmeier@gmx.de>
2021-01-04 22:58:09 +0100

    test for reference finder

--- tests/test_reffinder.py
+++ tests/test_reffinder.py
@@ -9,3 +9,24 @@ def test_1():
     finder = RefFinder(ref_replacer_1)
     finder.replace_in(testblocks1[0])
     assert testblocks1[0]['content'] == '<a href="#hallo">Hallo</a>'
+
+
+
+testblocks2 = [{'content': '<@hallo>'},{'id':'hallo'}]
+def ref_replacer_2(ref,name):
+    found = next((x for x in testblocks2 if x['id'] == ref))
+    return '<a href="#{}">{}</a>'.format(ref,found['name'])
+def test_2():
+    finder = RefFinder(ref_replacer_1)
+    finder.replace_in(testblocks1[0])
+    assert testblocks2[0]['content']
+
+
+
+def ref_replacer_3(ref,name):
+    return '<a href="#{}">{}</a>'.format(ref,name)
+testblocks3 = [{'content': '<@hallo|Buono>'}]
+def test_3():
+    finder = RefFinder(ref_replacer_3)
+    finder.replace_in(testblocks3[0])
+    assert 'Buono' in testblocks3[0]['content']
